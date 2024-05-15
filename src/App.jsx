aa61bf640b7ca2ad5182984c25371006a31214de
lukemandavid4 <lukemandavid4@gmail.com>
2024-05-15 22:18:50 +0300

    updated

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <div className="container__top">
+        <div className="container__top--center">
+          <h1>Poverty is not God&apos;s <br /> plan. You are.</h1>
+          <p>Your gift will tranform lives and <br /> play a part in ending extreme poverty <br /> and injustice around the world.</p>
+          <button type="button">Donate now</button>
+        </div>
+      </div>
+      <div className="container__bottom"></div>
+    </div>
+  )
+}
+
+export default App
